368971ffa7f3ee9ff3efc984b10ab091fcfc7a30
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-12-05 10:31:56 +0700

    fix: don't use max CPU

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "atlasprotocol",
       script: "npm",
       args: "start",
-      instances: "max", // You can specify the number of instances, 'max' means PM2 will start as many as your CPU cores
       autorestart: true, // Restart the app if it crashes
       env: {
         NODE_ENV: "production",
