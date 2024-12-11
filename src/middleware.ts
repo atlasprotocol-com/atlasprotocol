8529ae15a540fa5a2c14798c4699f53e16d2c0e3
Giang Nguyen <giang.nguyen.dev@gmail.com>
2024-12-11 22:14:02 +0700

    rm unsafe inline

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ""};
-    style-src 'self' 'unsafe-inline';
+    style-src 'self';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
