59c982409e19fa02ff7fd24330430f8573481d87
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2025-02-15 15:08:26 +0700

    fix: CSP

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
 
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ""};
+    script-src 'self' 'nonce-${nonce}' ${isDev ? "'unsafe-eval'" : ""};
     style-src 'self';
     img-src 'self' blob: data:;
     font-src 'self';
