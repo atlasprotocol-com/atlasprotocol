d814ca5e2355234298775611efdbec64da20fcf1
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2025-03-16 10:36:19 +0700

    fix: default value of BTC_MIN_CONFIRMATIONS

--- backend/utils/updateAtlasBtcDeposited.js
+++ backend/utils/updateAtlasBtcDeposited.js
@@ -2,11 +2,9 @@ const { getConstants } = require("../constants");
 
 const { flagsBatch } = require("./batchFlags");
 
-async function UpdateAtlasBtcDeposited(
-  depositRecords,
-  near,
-  bitcoinInstance,
-) {
+const BTC_MIN_CONFIRMATIONS = Number(process.env.BTC_MIN_CONFIRMATIONS || 1);
+
+async function UpdateAtlasBtcDeposited(depositRecords, near, bitcoinInstance) {
   const batchName = `Batch B UpdateAtlasBtcDeposited`;
   console.log(batchName);
 
@@ -22,10 +20,10 @@ async function UpdateAtlasBtcDeposited(
     const currentBlockHeight = await bitcoinInstance.getCurrentBlockHeight();
     console.log("Current block height:", currentBlockHeight);
 
-    
-    const filteredRecords = depositRecords.filter(record => 
-      record.status === DEPOSIT_STATUS.BTC_PENDING_DEPOSIT_MEMPOOL && 
-      !record.remarks
+    const filteredRecords = depositRecords.filter(
+      (record) =>
+        record.status === DEPOSIT_STATUS.BTC_PENDING_DEPOSIT_MEMPOOL &&
+        !record.remarks,
     );
 
     console.log("filteredRecords:", filteredRecords);
@@ -37,17 +35,27 @@ async function UpdateAtlasBtcDeposited(
       try {
         // Get transaction details from mempool
         const txn = await bitcoinInstance.fetchRawTransaction(btcTxnHash);
-       
-        if(txn && txn.status.block_height && txn.status.confirmed){
-          console.log("txn_hash:", txn.txid, " confirmations:", currentBlockHeight - txn.status.block_height + 1);
-          
-          if (txn && txn.status.confirmed && (currentBlockHeight - txn.status.block_height + 1) >= process.env.BTC_MIN_CONFIRMATIONS) {
+
+        if (txn && txn.status.block_height && txn.status.confirmed) {
+          console.log(
+            "txn_hash:",
+            txn.txid,
+            " confirmations:",
+            currentBlockHeight - txn.status.block_height + 1,
+          );
+
+          if (
+            txn &&
+            txn.status.confirmed &&
+            currentBlockHeight - txn.status.block_height + 1 >=
+              BTC_MIN_CONFIRMATIONS
+          ) {
             console.log("btcTxnHash confirmed:", btcTxnHash);
             timestamp = txn.status.block_time;
 
             // Update existing record
             await near.updateDepositBtcDeposited(btcTxnHash, timestamp);
-            
+
             console.log(`Updated Deposit with BTC txn hash ${btcTxnHash}`);
           }
         }
@@ -67,4 +75,4 @@ async function UpdateAtlasBtcDeposited(
   }
 }
 
-module.exports = { UpdateAtlasBtcDeposited }; 
+module.exports = { UpdateAtlasBtcDeposited };
