a55bdcc0e0085dd1a58ce4be1b701dbc36effc76
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2025-06-25 21:35:35 +0700

    chore: debug

--- contract/src/modules/migrate.rs
+++ contract/src/modules/migrate.rs
@@ -48,7 +48,7 @@ impl Atlas {
     #[init(ignore_state)]
     pub fn migrate_init() -> Self {
         let x = env::storage_read(STATE_KEY).unwrap();
-        env::log_str(&x.len().to_string());
+        env::log_str(hex::encode(&x));
         let old_state: V2 = env::state_read().expect("Failed to read old state");
 
         let data = match borsh::to_vec(&old_state) {
