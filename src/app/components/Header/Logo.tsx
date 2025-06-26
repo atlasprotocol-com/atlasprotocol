7e43cc9bda240ef438a6fe0ac019b5a45d40d729
MTae <flypy2902@gmail.com>
2025-06-26 18:33:50 +0700

    wip: rebrand change logo

--- src/app/components/Header/Logo.tsx
+++ src/app/components/Header/Logo.tsx
@@ -1,31 +1,20 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-
-import baseLogoBlack from "@/app/assets/base-logo-black.svg";
-import baseLogo from "@/app/assets/base-logo.svg";
 
 interface LogoProps {}
 
 export const Logo: React.FC<LogoProps> = () => {
-  const { resolvedTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-  const lightSelected = resolvedTheme === "light";
-  const logo = lightSelected ? baseLogoBlack : baseLogo;
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
   return (
     <div className="flex items-center">
-      <Image src={logo} alt="Atlas" height={40} loading="lazy" />
+      <Image
+        src="/logo_text.svg"
+        alt="Atlas Protocol"
+        height={32}
+        width={150}
+        className="h-8 w-auto transition-all duration-200 hover:opacity-90 md:h-10 md:w-auto"
+        priority
+      />
     </div>
   );
 };
